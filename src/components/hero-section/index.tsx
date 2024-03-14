6ea90fecd909b62ce298608e202c3d67185d8fa3
saankalpaa <saankalpaa@gmail.com>
2024-03-14 07:10:59 +0545

    makes hero section mobile responsive

--- src/components/hero-section/index.tsx
+++ src/components/hero-section/index.tsx
@@ -1,10 +1,9 @@
 import React from "react";
+import styles from "./hero.module.css";
+import Image from "next/image";
 
 import CTAButton from "components/cta-button";
-import styles from "./hero.module.css";
-import Link from "next/link";
 import ButterLogo from "../../assets/icons/butter.svg";
-import Image from "next/image";
 
 export default function Hero(): React.ReactElement {
   return (
@@ -12,29 +11,29 @@ export default function Hero(): React.ReactElement {
       <h1 className={styles.heading}>
         Content growth can happen over a month. And, we can prove that!
       </h1>
-      <p>You focus on your product, we got your marketing!</p>
-      <div className={styles.butterLogo}>
-        <Image
-          width={32}
-          src={ButterLogo}
-          alt="Butter Logo"
-        />
-      </div>
+      <div className={styles.descContainer}>
+        <p>You focus on your product, we got your marketing!</p>
 
-      <div className={styles.button_container}>
+        <div className={styles.butterLogo}>
+          <Image
+            width={32}
+            src={ButterLogo}
+            alt="Butter Logo"
+          />
+        </div>
+      </div>
+      <div className={styles.buttonContainer}>
         <CTAButton
-          className={styles.cta_button}
+          className={styles.ctaButton}
           link="https://calendly.com/reduct-nepal/good-better-butter-your-content"
         >
           Reach us out today!
         </CTAButton>
-        <CTAButton className={styles.pricing_button}>
-          <Link
-            href="/#pricing"
-            style={{ textDecoration: "none", color: "#fff" }}
-          >
-            Get Pricing
-          </Link>
+        <CTAButton
+          className={styles.pricingButton}
+          link="/#pricing"
+        >
+          Get Pricing
         </CTAButton>
       </div>
     </div>
