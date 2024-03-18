a9c1a29bd7bc5ad81ca314c145ac4365a974f864
saankalpaa <saankalpaa@gmail.com>
2024-03-18 16:28:23 +0545

    adds pricing section

---
+++ src/components/pricing/Card.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import styles from "./card.module.css";
+import Image, { StaticImageData } from "next/image";
+import CTAButton from "../CtaButton";
+
+interface PricingCopyInterface {
+  title: string;
+  titleIcon?: StaticImageData;
+  description?: string;
+  pricing?: string;
+  pricingCriteria?: React.ReactElement;
+  ctaText: string;
+  whatsIncludedList?: Array<string>;
+  teamEquivalent?: string;
+}
+
+export default function Card({
+  copy,
+  className,
+  ctaClassName,
+}: {
+  copy: PricingCopyInterface;
+  className?: string;
+  ctaClassName?: string;
+}): React.ReactElement {
+  const {
+    title,
+    titleIcon,
+    description,
+    pricing,
+    pricingCriteria,
+    ctaText,
+    whatsIncludedList,
+    teamEquivalent,
+  } = copy;
+
+  return (
+    <div className={`${styles.card} ${className}`}>
+      <div className={styles.topContainer}>
+        <h4 className={styles.header}>
+          {title}
+          {titleIcon && (
+            <span>
+              <Image
+                src={titleIcon}
+                alt="Butter Growth Pricing Title Icon"
+              />
+            </span>
+          )}
+        </h4>
+        {description && <p>{description}</p>}
+        <div className={styles.cardPricing}>
+          <h1>{pricing}</h1>
+          {pricingCriteria && pricingCriteria}
+        </div>
+        <CTAButton
+          link="https://calendly.com/reduct-nepal/good-better-butter-your-content"
+          className={ctaClassName}
+        >
+          {ctaText}
+        </CTAButton>
+      </div>
+      {whatsIncludedList?.length && (
+        <div className={styles.secondaryContainer}>
+          <p className={styles.secondaryHeader}>What’s included?</p>
+          <div className={styles.whatsIncludedList}>
+            {whatsIncludedList.map((d) => (
+              <p key={d}>{d}</p>
+            ))}
+          </div>
+        </div>
+      )}
+      {teamEquivalent && (
+        <div className={styles.secondaryContainer}>
+          <p className={styles.secondaryHeader}>Team Equivalent</p>
+          <p>{teamEquivalent}</p>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/components/pricing/index.tsx
+++ src/components/pricing/index.tsx
@@ -1,5 +1,37 @@
 import React from "react";
+import styles from "./pricing.module.css";
+
+import Card from "./Card";
+import {
+  PRICING_GROWTH_COPY,
+  ONE_OFF_BLOG_COPY,
+  CUSTOM_PRICING_COPY,
+} from "./constant";
 
 export default function Pricing(): React.ReactElement {
-  return <div>Pricing</div>;
+  return (
+    <section className="container">
+      <div className={`${styles.pricingContainer} section-container`}>
+        <h1>
+          Pricing<span className="butter-text">.</span>
+        </h1>
+        <div className={styles.pricingCardContainer}>
+          <Card
+            copy={PRICING_GROWTH_COPY}
+            className={styles.cardFit}
+          />
+          <div className={styles.verticalCardContainer}>
+            <Card
+              copy={ONE_OFF_BLOG_COPY}
+              ctaClassName={styles.transparentCta}
+            />
+            <Card
+              copy={CUSTOM_PRICING_COPY}
+              ctaClassName={styles.transparentCta}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
