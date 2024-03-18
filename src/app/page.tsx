fe9f8ce39d1668c3cb1bf27f51ae2880b70e4be2
Ashish-Shikhrakar <shikhrakar.e@gmail.com>
2024-03-18 15:49:19 +0545

    Add problem section below hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,14 @@ import Hero from "../components/HeroSection";
 import Navbar from "root/components/NavBar";
 import ButterWay from "components/ButterWay";
 import { SolutionSectionCopy } from "root/components/ButterWay/constant";
+import Problem from "root/components/ProblemSection";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar />
       <Hero />
+      <Problem />
       <ButterWay copy={SolutionSectionCopy} />
     </main>
   );
