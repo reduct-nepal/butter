c16691b067dc058644a8fb056b8538eec33a33b4
saankalpaa <saankalpaa@gmail.com>
2024-03-19 07:41:31 +0545

    adds favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="./favicon.ico"
+          sizes="any"
+        />
+      </head>
       <body>
         <main className={ubuntu.className}>{children}</main>
       </body>
